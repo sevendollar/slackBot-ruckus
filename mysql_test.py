207d3fe48e360f567f19ffa946fc92be543f2b2d
jeffl <sevendollar@gmail.com>
2018-05-08 09:39:45 +0800

    support csv & check if the MAC existed the the DB and give a response.

--- mysql_test.py
+++ mysql_test.py
@@ -1,4 +1,5 @@
 import pymysql
+import csv
 
 
 class Sql:
@@ -8,7 +9,11 @@ class Sql:
         self.user = user
 
     def __enter__(self):
-        self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user)
+        self.conn = pymysql.connect(host=self.host,
+                                    port=self.port,
+                                    user=self.user,
+                                    use_unicode=True,
+                                    charset='utf8',)
         self.cur = self.conn.cursor()
         print('db connection started!')
         return self.conn, self.cur
@@ -22,8 +27,7 @@ class Sql:
 
 db = 'jefDB'
 table = 'jefTABLE'
-fields = ('FirstName', 'LastName', 'Sex', 'Skill', 'Age')
-values = ('Jef', 'Lee', 'M', 'python', 18,)
+fields = ('TeamName', 'TeamUser', 'CustermerName', 'ID', 'MAC')
 
 sql_use_db = f'use {db};'
 sql_create_db = f'create database {db};'
@@ -32,13 +36,13 @@ sql_is_table_exist = f'show tables like \'{table}\';'
 sql_create_table = f'''create table {db}.{table}(
                         {fields[0]} CHAR(20),
                         {fields[1]} CHAR(20),
-                        {fields[2]} CHAR(1),
+                        {fields[2]} CHAR(20),
                         {fields[3]} CHAR(20),
-                        {fields[4]} INT);'''
-sql_insert = f'''insert into {db}.{table}({', '.join(fields)}) values{values};'''
+                        {fields[4]} CHAR(20) PRIMARY KEY);'''
+# sql_insert = f'''insert into {db}.{table}({', '.join(fields)}) values{values};'''
 
 if __name__ == '__main__':
-    with Sql('10.7.12.65', 32772) as (conn, cur):
+    with Sql('10.7.12.65', 32785) as (conn, cur):
         try:
             if not cur.execute(sql_is_db_exist):  # create DB only if it doesn't exist.
                 cur.execute(sql_create_db)
@@ -50,7 +54,13 @@ if __name__ == '__main__':
                 if not cur.execute(sql_is_table_exist):  # create TABLE only if it doesn't exist.
                     cur.execute(sql_create_table)
             if cur.execute(sql_is_db_exist) and cur.execute(sql_is_table_exist):
-                cur.execute(sql_insert)
-                conn.commit()
+                with open('Book1.csv', 'r') as csv_file:
+                    for value in csv.reader(csv_file):  # loop through the whole csv.
+                        # insert records only if it's not duplicated in the DB.
+                        if not cur.execute(f'select MAC from {db}.{table} where MAC = \'{value[4]}\';'):
+                            cur.execute(f'''insert into {db}.{table}({', '.join(fields)}) values{tuple(value)};''')
+                        else:
+                            print(f'{value[4]} existed!')
+                    conn.commit()
         except pymysql.err.InternalError as E:
             print(E)
