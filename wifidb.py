89098a493a85e2a945060c950736511a5dc84d2d
Roycechen <roycechen68@gmail.com>
2018-07-27 10:02:03 +0800

    new_key

--- wifidb.py
+++ wifidb.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 import pymysql
 
+
 class Sql:
     def __init__(self, host='localhost', port=3306, user='royce', password=None, db=None):
         self.host = host
@@ -32,21 +33,19 @@ pw = 'Royce898O4142'
 host = '10.5.1.113'
 
 fields = ('team_name', 'team_user', 'customer_name', 'customer_id', 'mac')
+new_key = ['team_name','team_user','customer_name','customer_id','mac']
 new_value = []
-new_key = []
 
 sql_use_db = f'use {db};'
 sql_create_db = f'create database {db};'
 sql_is_db_exist = f'show databases like \'{db}\';'
 sql_is_table_exist = f'show tables like \'{table}\';'
 sql_create_table = f'''create table {db}.{table}(
-                        {fields[0]} CHAR(20),
+                        {fields[0]} CHAR(20),  
                         {fields[1]} CHAR(20),
                         {fields[2]} CHAR(20),
                         {fields[3]} CHAR(20),
                         {fields[-1]} CHAR(20) PRIMARY KEY);'''
-# sql_insert = f'''insert into {db}.{table}({', '.join(fields)}) values{values};'''
-
 
 def InsertData(new_words):
     with Sql(host=host, port=port, password=pw) as (conn, cur):
@@ -62,22 +61,21 @@ def InsertData(new_words):
                     cur.execute(sql_create_table)
             if cur.execute(sql_is_db_exist) and cur.execute(sql_is_table_exist):
                 for key, value in new_words.items():
-                    new_key.append(key)
                     new_value.append(value)
-                    K = ','.join(i for i in new_key)
+                    K = ', '.join(new_key)
                     V = ','.join("'" + i + "'" for i in new_value)
                 if not cur.execute(f'select mac from {db}.{table} where mac = \'{new_value[-1]}\';'):
                     cur.execute(f'''INSERT INTO {db}.{table}({K})  VALUES ({V});''')
                     conn.commit()
                     if cur.execute(f'select mac from {db}.{table} where mac = \'{new_value[-1]}\';'):
-                        #print(f'成功新增 {new_value[-1]}至資料庫!')
+                        print(f'成功新增 {new_value[-1]}至資料庫!')
                         return True
                     else:
-                        #print('資料寫入失敗！')
-                        return False
-                else:
-                    #print(f'{new_value[-1]}已存在!')
-                    return False
+                        print('資料寫入失敗！')
+                        return False or None
+            else:
+                print(f'{new_value[-1]}已存在!')
+                return False or None
         except pymysql.err.InternalError as E:
             print(E)
 
